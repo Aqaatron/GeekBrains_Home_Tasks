708945f265648c65d23b6a368fb144a98ab52c31
aqaatron <aqaatron@gmail.com>
2021-02-07 21:45:15 +0300

    Пункт 2

---
+++ prac4/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string numbers = Console.ReadLine();
+
+            double sum = 0; ;
+
+            string[] num  = numbers.Split(' ');
+
+            foreach (var i in num)
+            {
+                 sum = sum + Convert.ToDouble(i);
+            }
+            Console.WriteLine(sum);
+
+            Console.ReadLine();
+        }
+    }
+}
